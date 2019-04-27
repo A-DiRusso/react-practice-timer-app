487b61c5818f97a98b8164d7fa9c2c520d4ee825
Anthony DiRusso <ossurid@gmail.com>
2019-04-27 09:29:50 -0400

    fixesbug :)

--- src/Timer.js
+++ src/Timer.js
@@ -34,31 +34,31 @@ class Timer extends Component {
     }
     _timerClick = () => {
         if (this.state.status) {
-            clearInterval(this.interval)
-            this.setState({
-                status: false
-            })
+          clearInterval(this.interval)
+          this.setState({
+            status: false,
+          }) 
         } else {
-            this.setState({
-                status: true,
-                completion: false
-            })
-            this.interval = setInterval(() => {
-                if (this.state.time >= 1) {
-                    this.setState({
-                        time: this.state.time - 1
-                    })
-                } else {
-                    this.setState({
-                        time: this.state.input,
-                        status: false,
-                        input: this.state.input,
-                        completion: true
-                    }, 1000)
-                }
-            })
+          this.setState({
+            status: true,
+            completion: false
+          })
+          this.interval = setInterval(() => {
+            if (this.state.time >= 1) {
+              this.setState({
+                time: this.state.time - 1,
+              })
+            } else {
+              this.setState({
+                time: this.state.input,
+                status: false,
+                input: this.state.input,
+                completion: true
+              }, clearInterval(this.interval))
+            }
+          }, 1000)
         }
-    }
+      }
     _resetClick = () => {
         clearInterval(this.interval)
         this.setState({
